99b0014ec103fdd81158412963d76362f776b016
Maciej Malecki <malecki.maciej@gmail.com>
2021-07-20 12:55:24 +0200

    enable entities

--- configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/SpringbootApplication.java
+++ configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/SpringbootApplication.java
@@ -2,12 +2,14 @@ package io.github.hexarchtraining.hts.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "io.github.hexarchtraining.hts.springboot",
        "io.github.hexarchtraining.hts.booking.adapter.in.springweb"})
 @EnableJpaRepositories(basePackages = "io.github.hexarchtraining.hts.bookings.adapter.out.jpa")
+@EntityScan(basePackages = "io.github.hexarchtraining.hts.bookings.adapter.out.jpa")
 public class SpringbootApplication {
 
     public static void main(String[] args) {
