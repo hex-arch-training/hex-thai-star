19a65f3dec23850a5d892f8ee512d278fb96e7c0
Maciej Malecki <malecki.maciej@gmail.com>
2021-07-22 11:26:09 +0200

    Controller advice

---
+++ booking/booking-adapter-in-springweb/src/main/java/io/github/hexarchtraining/hts/booking/adapter/in/springweb/ErrorHandler.java
@@ -0,0 +1,33 @@
+package io.github.hexarchtraining.hts.booking.adapter.in.springweb;
+
+import io.github.hexarchtraining.hts.booking.common.exception.NotFoundException;
+import io.github.hexarchtraining.hts.booking.domain.exception.BookingValidationException;
+import io.github.hexarchtraining.hts.booking.domain.exception.IllegalBookingStateException;
+import io.github.hexarchtraining.hts.booking.domain.exception.IncompleteBookingException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(NotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(BookingValidationException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(IncompleteBookingException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handle(IllegalBookingStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
