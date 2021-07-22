1a842e0b3c41b0d411c99aebbd47a3a301c83cd7
Maciej Malecki <malecki.maciej@gmail.com>
2021-07-22 13:39:21 +0200

    clean up on transactions

--- configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/adapter/out/SendBookingConfirmationDummyAdapter.java
+++ configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/adapter/out/SendBookingConfirmationDummyAdapter.java
@@ -1,11 +1,11 @@
-package io.github.hexarchtraining.hts.springboot.configuration;
+package io.github.hexarchtraining.hts.springboot.adapter.out;
 
 import io.github.hexarchtraining.hts.booking.port.out.BookingConfimationEvent;
 import io.github.hexarchtraining.hts.booking.port.out.SendBookingConfirmationPort;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SendBookingConfirmationAdapterOut implements SendBookingConfirmationPort {
+public class SendBookingConfirmationDummyAdapter implements SendBookingConfirmationPort {
     @Override
     public void send(BookingConfimationEvent event) {
         // just to fulfil dependency, normally we would like to dispatch an event to notification component
