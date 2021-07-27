f3cbfe1fb9079921e822554648c852ec9b701c1f
Maciej Malecki <malecki.maciej@gmail.com>
2021-07-27 23:09:09 +0200

    tx jdbi

--- booking/booking-adapter-out-jdbi/src/main/java/io/github/hexarchtraining/hts/booking/adapter/out/jdbi/dao/BookingDao.java
+++ booking/booking-adapter-out-jdbi/src/main/java/io/github/hexarchtraining/hts/booking/adapter/out/jdbi/dao/BookingDao.java
@@ -1,12 +1,13 @@
 package io.github.hexarchtraining.hts.booking.adapter.out.jdbi.dao;
 
 import io.github.hexarchtraining.hts.booking.adapter.out.jdbi.record.BookingRecord;
+import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 import java.util.Optional;
 
 public interface BookingDao {
 
-    @SqlQuery("SELECT * FROM Booking WHERE token = ?")
-    Optional<BookingRecord> findBookingByToken(String token);
+    @SqlQuery("SELECT * FROM Booking WHERE token = :token")
+    Optional<BookingRecord> findBookingByToken(@Bind("token") String token);
 }
--- booking/booking-adapter-out-jdbi/src/main/java/io/github/hexarchtraining/hts/booking/adapter/out/jdbi/dao/TableBookingDao.java
+++ booking/booking-adapter-out-jdbi/src/main/java/io/github/hexarchtraining/hts/booking/adapter/out/jdbi/dao/TableBookingDao.java
@@ -1,9 +1,10 @@
 package io.github.hexarchtraining.hts.booking.adapter.out.jdbi.dao;
 
+import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface TableBookingDao {
 
-    @SqlUpdate("DELETE FROM Table_Booking WHERE booking_id=? AND table_id=?")
-    int deleteTableBooking(long bookingId, long tableId);
+    @SqlUpdate("DELETE FROM Table_Booking WHERE booking_id=:bookingId AND table_id=:tableId")
+    void deleteTableBooking(@Bind("bookingId") long bookingId, @Bind("tableId") long tableId);
 }
