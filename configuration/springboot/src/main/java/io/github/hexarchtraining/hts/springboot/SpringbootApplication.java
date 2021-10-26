5e597c3e24107f34737e308e451a3a36bbcb29c4
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2021-10-26 11:28:03 +0200

    [mail-notification-component_23] Mail notification component for spring boot

--- configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/SpringbootApplication.java
+++ configuration/springboot/src/main/java/io/github/hexarchtraining/hts/springboot/SpringbootApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
-        "io.github.hexarchtraining.hts.springboot",
-       "io.github.hexarchtraining.hts.booking.adapter.in.springweb"})
+    "io.github.hexarchtraining.hts.springboot",
+    "io.github.hexarchtraining.hts.booking.adapter.in.springweb",
+    "io.github.hexarchtraining.hts.booking.adapter.out.springmail"})
 @EnableJpaRepositories(basePackages = "io.github.hexarchtraining.hts.booking.adapter.out.jpa")
 @EntityScan(basePackages = "io.github.hexarchtraining.hts.booking.adapter.out.jpa")
 public class SpringbootApplication {
